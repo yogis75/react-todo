9ad4bf7ddb5d133bbf020c6855c41fb501adbc21
yogi.s75 <yogi.s75@gmail.com>
2022-06-28 07:04:14 +0530

    html and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+//Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Jings Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
